2b8d7e816168956dd64ca882d7b8cef3c7e330f0
Robert Servado <servadorobertgmail.com>
2022-11-13 23:03:49 -0500

    Modified the src/App.js to include more functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Fragment } from "react";
 import "./App.css";
 
+//components
+
+import EnterToDo from "./components/EnterToDo";
+import ListTodos from "./components/ListTodos";
+
 function App() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <div className="container">
+        <EnterToDo />
+        <ListTodos />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
